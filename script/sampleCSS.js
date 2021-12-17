a0b4d55eb1a475313db81b304fe4b98c37412ff0
eina5910315 <eina5920409@gmail.com>
2021-12-17 09:38:14 +0900

    スクロール追加、README修正

--- script/sampleCSS.js
+++ script/sampleCSS.js
@@ -1,6 +1,32 @@
+function init() {
+    scrolleAnimation();
+}
+
+function scrolleAnimation() {
+    const l = document.querySelectorAll(".scrolleAnimation");
+    const margin = 150;
+    console.log(l);
+    window.addEventListener("scroll", () => {
+        l.forEach(t => {
+            const rect = t.getBoundingClientRect().top;
+            const scroll = window.pageYOffset || document.documentElement.scrollTop;
+            const offset = rect + scroll;
+            const windowHeight = window.innerHeight;
+            if (scroll > offset - windowHeight + margin) {
+                t.classList.add('animation');
+            }
+        });
+    });
+}
+
 async function restertAnimation(target) {
     const t = document.getElementById(target);
     t.classList.remove("animation");
     await new Promise(resolve => setTimeout(resolve, 100))
     t.classList.add("animation");
+}
+
+function deleteAnimation(target) {
+    const t = document.getElementById(target);
+    t.classList.remove("animation");
 }
